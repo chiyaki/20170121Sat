e8cd8a0fec23722776a4f573a88486b35745ef2d
chiyaki <66chiyaki@gmail.com>
2017-01-25 16:16:50 +0800

    ldpage basic columns

---
+++ db/migrate/20170125074330_add_texts_to_ldpage.rb
@@ -0,0 +1,34 @@
+class AddTextsToLdpage < ActiveRecord::Migration[5.0]
+  def change
+    add_column :ldpages, :text01, :text
+    add_column :ldpages, :text02, :text
+    add_column :ldpages, :text03, :text
+    add_column :ldpages, :text04, :text
+    add_column :ldpages, :text05, :text
+    add_column :ldpages, :text06, :text
+    add_column :ldpages, :text07, :text
+    add_column :ldpages, :text08, :text
+    add_column :ldpages, :text09, :text
+    add_column :ldpages, :text10, :text
+    add_column :ldpages, :text11, :text
+    add_column :ldpages, :text12, :text
+    add_column :ldpages, :text13, :text
+    add_column :ldpages, :text14, :text
+    add_column :ldpages, :text15, :text
+    add_column :ldpages, :text16, :text
+    add_column :ldpages, :text17, :text
+    add_column :ldpages, :text18, :text
+    add_column :ldpages, :text19, :text
+    add_column :ldpages, :text20, :text
+    add_column :ldpages, :text21, :text
+    add_column :ldpages, :text22, :text
+    add_column :ldpages, :text23, :text
+    add_column :ldpages, :text24, :text
+    add_column :ldpages, :text25, :text
+    add_column :ldpages, :text26, :text
+    add_column :ldpages, :text27, :text
+    add_column :ldpages, :text28, :text
+    add_column :ldpages, :text29, :text
+    add_column :ldpages, :text30, :text
+  end
+end
