c48b4c72b4594b8408bfa1cbb875caf244ed27cd
chiyaki <66chiyaki@gmail.com>
2017-01-26 20:19:27 +0800

    implement new action

--- app/controllers/ldpages_controller.rb
+++ app/controllers/ldpages_controller.rb
@@ -1,2 +1,41 @@
 class LdpagesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  layout 'blank'
+  def edit
+    render :template => "templates/default"
+  end
+
+  def new
+    @ldpage = Ldpage.new
+  end
+
+  def create
+    @ldpage = Ldpage.new(ldpage_params)
+    @ldpage.user = current_user
+    if @ldpage.save
+      redirect_to edit_ldpage_path(@ldpage)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @ldpage = Ldpage.find(params[:id])
+    if @ldpage.update(ldpage_params)
+      redirect_to ldpage_path(@ldpage)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def ldpage_params
+    params.require(:ldpage).permit(:text01, :text02, :text03, :text04, :text05, :text06, :text07, :text08, :text09, :text10,
+                                   :text11, :text12, :text13, :text14, :text15, :text16, :text17, :text18, :text19, :text20,
+                                   :text21, :text22, :text23, :text24, :text25, :text26, :text27, :text28, :text29, :text30,
+                                   :image01, :image02, :image03, :image04, :image05, :image06, :image07, :image08, :image09, :image10,
+                                   :image11, :image12, :image13, :image14, :image15, :image16, :image17, :image18, :image19, :image20,
+                                   :image21, :image22, :image23, :image24, :image25, :image26, :image27, :image28, :image29, :image30,
+                                   :user_id)
+  end
 end
--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,3 +1,13 @@
 class TemplatesController < ApplicationController
-  
+  # def index
+  # end
+  #
+  # def use_this_template
+  #   @ldpage = Ldpage.new(ldpage_params)
+  #   if @ldpage.save
+  #     redirect_to edit_ldpage_path(@ldpage)
+  #   else
+  #     render :new
+  #   end
+  # end
 end
