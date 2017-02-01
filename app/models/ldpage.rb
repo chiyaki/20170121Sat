dde7ac5e77aa465b28fd0d613003371b9eaa4daf
chiyaki <66chiyaki@gmail.com>
2017-02-01 23:45:55 +0800

    用户可以上传图片到landing page

--- app/models/ldpage.rb
+++ app/models/ldpage.rb
@@ -2,4 +2,14 @@ class Ldpage < ApplicationRecord
   belongs_to :user
   belongs_to :template
   scope :recent, -> { order('created_at DESC') }
+  mount_uploader :image01, Image01Uploader
+  mount_uploader :image02, Image02Uploader
+  mount_uploader :image03, Image03Uploader
+  mount_uploader :image04, Image04Uploader
+  mount_uploader :image05, Image05Uploader
+  mount_uploader :image06, Image06Uploader
+  mount_uploader :image07, Image07Uploader
+  mount_uploader :image08, Image08Uploader
+  mount_uploader :image09, Image09Uploader
+  mount_uploader :image10, Image10Uploader
 end
