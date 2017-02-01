327fff523a0c6314edad9472fb6e59666a39e14f
chiyaki <66chiyaki@gmail.com>
2017-02-01 14:55:30 +0800

    add seed & develop record on redmine

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Template.create
+Template.create
+puts "generate two template id"
+
+User.create([email: "user1@gmail.com", password: "12345678", password_confirmation: "12345678"])
+puts "generate a user"
